46de95544e7ea3a409ae6871f7f83bfcd3572f49
Xylope <XylopeOfficial@gmail.com>
2021-01-17 13:49:34 +0900

    practice 001 - 유저 모델 및 DAO 구현

---
+++ src/main/java/com/xylope/toby_spring_practice/user/dao/UserDao.java
@@ -0,0 +1,51 @@
+package com.xylope.toby_spring_practice.user.dao;
+
+import com.xylope.toby_spring_practice.user.domain.User;
+
+import java.sql.*;
+
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/toby_spring_practice?serverTimezone=Asia/Seoul", "root", "0000"
+        );
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users (id, name, password) values(?,?,?)"
+        );
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/toby_spring_practice?serverTimezone=Asia/Seoul", "root", "0000"
+        );
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?"
+        );
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+}

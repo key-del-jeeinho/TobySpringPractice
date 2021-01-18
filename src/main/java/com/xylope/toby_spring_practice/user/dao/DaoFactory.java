90b75d2e6813d59810260f6ff888e72eefc63e70
Xylope <XylopeOfficial@gmail.com>
2021-01-19 02:22:46 +0900

    프레임워크 추가 - SpringFramework Spring 5.2.3 리팩터링 - 팩토리 클래스를 어노테이션을 사용해 ApplicationContext 로 전환

--- src/main/java/com/xylope/toby_spring_practice/user/dao/DaoFactory.java
+++ src/main/java/com/xylope/toby_spring_practice/user/dao/DaoFactory.java
@@ -1,12 +1,18 @@
 package com.xylope.toby_spring_practice.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         ConnectionMaker connectionMaker = new DConnectionMaker();
         return new UserDao(connectionMaker);
     }
 
-    private ConnectionMaker connectionMaker() {
+    @Bean
+    public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
 }
--- src/test/java/UserDAOTest.java
+++ src/test/java/UserDAOTest.java
@@ -2,12 +2,15 @@ import com.xylope.toby_spring_practice.user.dao.DConnectionMaker;
 import com.xylope.toby_spring_practice.user.dao.DaoFactory;
 import com.xylope.toby_spring_practice.user.dao.UserDao;
 import com.xylope.toby_spring_practice.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDAOTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("efgh123");

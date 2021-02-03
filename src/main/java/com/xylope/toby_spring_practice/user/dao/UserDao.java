edc1c19b50c1600902f64b2377955af46fcc9df1
Xylope <XylopeOfficial@gmail.com>
2021-02-03 09:48:16 +0900

    리펙터링 - 더 낮은 결합도를 유지하기위해서 JdbcTemplate 를 JdbcOperations 라는 인터페이스를 통해 전략패턴 사용. 이를 bean 으로 등록하여 구현 (참고로, JdbcTemplate 는 사실상 표준기술이기에 변화의 여부가 거의없으며, Dao 에 JdbcTemplate 을 작성하는것이 관례라고 책에서 나와있었으나, 다른 방법을 탐색해보기 위해 해당 패턴 채용)

--- src/main/java/com/xylope/toby_spring_practice/user/dao/UserDao.java
+++ src/main/java/com/xylope/toby_spring_practice/user/dao/UserDao.java
@@ -1,42 +1,39 @@
 package com.xylope.toby_spring_practice.user.dao;
 
 import com.xylope.toby_spring_practice.user.domain.User;
-import org.springframework.jdbc.core.JdbcTemplate;
+import lombok.Setter;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
 
-import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class UserDao {
-    private JdbcTemplate jdbcTemplate;
+    @Setter
+    private JdbcOperations jdbcOperations;
     private RowMapper<User> userMapper = (rs, rowNum) -> new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
 
-    public void setDataSource(DataSource dataSource) {
-        jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
     public void add(final User user) {
-        jdbcTemplate.update("insert into users (id, name, password) values (?, ?, ?)", user.getId(), user.getName(), user.getPassword());
+        jdbcOperations.update("insert into users (id, name, password) values (?, ?, ?)", user.getId(), user.getName(), user.getPassword());
     }
 
     public User get(String id) {
-        return jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id},
+        return jdbcOperations.queryForObject("select * from users where id = ?", new Object[]{id},
                 userMapper);
     }
 
     public ArrayList<User> getAll() {
-        return (ArrayList<User>) jdbcTemplate.query("select * from users order by id", userMapper);
+        return (ArrayList<User>) jdbcOperations.query("select * from users order by id", userMapper);
     }
 
     public void deleteAll() throws SQLException {
-        jdbcTemplate.update("delete from users");
+        jdbcOperations.update("delete from users");
     }
 
 
     public int getCount() {
         Integer count;
-        if((count = jdbcTemplate.queryForObject("select count(*) from users", Integer.class)) != null)
+        if((count = jdbcOperations.queryForObject("select count(*) from users", Integer.class)) != null)
             return count;
         return 0; //queryForObject return null
     }
